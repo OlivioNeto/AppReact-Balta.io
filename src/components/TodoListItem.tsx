7005eee0dc7df913acbc4f1a90f71f2d8c6362a7
Olivio Neto <netoolivio34@gmail.com>
2024-01-12 09:29:52 -0300

    Criando o onRemove e deixando pronto para a exclusão

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -7,9 +7,10 @@ interface TodoListItemProps{
 }
 
 const TodoListItem = (props: TodoListItemProps) => {
-
+  const onRemove = (todo: Todo) => {
+    console.log(todo);
+  }
   const handleChange = (event: any) =>{
-    event.target //para ver o evento que aconteceu
     console.log('Mudou');
   }
   return (
@@ -22,7 +23,8 @@ const TodoListItem = (props: TodoListItemProps) => {
         </td>
         <td className="uk-width-expand">{props.todo.title}</td>
         <td className="uk-width-auto">
-            <button className="uk-icon-button uk-button-danger" uk-icon="trash"></button>
+            <button className="uk-icon-button uk-button-danger" 
+            uk-icon="trash" onClick={() => onRemove(props.todo)}></button>
         </td>
     </tr>
   )
