c39a7b86af7df0c7fb5fba06b1365040c7d04127
Olivio Neto <netoolivio34@gmail.com>
2024-01-12 09:14:48 -0300

    Manipulando eventos através do handleChange e onChange

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -5,12 +5,19 @@ import { Todo } from '../models/Todo';
 interface TodoListItemProps{
     todo: Todo
 }
+
 const TodoListItem = (props: TodoListItemProps) => {
+
+  const handleChange = (event: any) =>{
+    event.target //para ver o evento que aconteceu
+    console.log('Mudou');
+  }
   return (
     <tr className="uk-animation-slide-bottom-medium">
         <td className="uk-width-auto">
             <label>
-                <input className="uk-checkbox" type="checkbox"/>
+                <input className="uk-checkbox" type="checkbox" 
+                checked={props.todo.done} onChange={handleChange}/>
             </label>
         </td>
         <td className="uk-width-expand">{props.todo.title}</td>
